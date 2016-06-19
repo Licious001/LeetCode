26e95ecea004a70b7f6e72cbd007d616af6f304c
李鑫 <licious@Licious-MacBook-Pro.local>
2016-06-19 11:30:16 +0800

    add Solution_4.h

--- main.cpp
+++ main.cpp
@@ -6,16 +6,15 @@
 //  Copyright (c) 2015年 李鑫. All rights reserved.
 //
 
-#include <iostream>
-#include <vector>
 //#include "solution_1.h"
 //#include "solution_2.h"
-#include "solution_3.h"
+//#include "solution_3.h"
+#include "solution_4.h"
 
 using namespace std;
 
 int main(int argc, char * argv[]) {
     Solution s;
-    cout << s.lengthOfLongestSubstring("abbbb") << endl;
+    cout << s.longestPalindrome("abbbb") << endl;
     return 0;
 }
