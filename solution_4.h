111573061dc73a4d544b596f7b5611667857f841
licious <licious@qq.com>
2016-06-19 23:28:31 +0800

    Solution_4 Done

--- solution_4.h
+++ solution_4.h
@@ -1,4 +1,3 @@
-
 // Soluiton_4.h
 // LeetCode
 
@@ -10,33 +9,54 @@
  * exists one unique longest palindromic substring.
  */
 
-#ifndef LeetCode_Solution_4_h
-#define LeetCode_Solution_4_h
+#ifndef LeetCode_solution_4_h
+#define LeetCode_solution_4_h
 
-#include<vector>
-#include<iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
 
 class Solution{
-public:
-    string longestPalindrome(string s){
-        
-        int length = s.length();
-        string reverse_s = "";
-        
-        for(int i = 0; i < length; i ++)
-            reverse_s = s[i] + reverse_s;
-       
-        //Find longest common substring for s and reverse_s
-        vector<int> pre_len;
-        vector<int> cur_len;
-        for(int i = 0; i < length; i ++){
-           for(int j = 0; j < length; j ++){
-           
-           } 
+    public:
+        string longestPalindrome(string s){
+
+            int length = s.length();
+            string reverse_s = "";
+
+            for(int i = 0; i < length; i ++)
+                reverse_s = s[i] + reverse_s;
+
+            //cout << s << endl;
+            //cout << reverse_s << endl;
+            //Find longest common substring for s and reverse_s
+            vector<int> pre_len(length, 0);
+            vector<int> cur_len(length, 0);
+
+            int max_pos = 0;
+            int max_len = 0;
+            for(int i = 0; i < length; i ++){
+                cur_len.assign(length,0);
+                for(int j = 0; j < length; j ++){
+                    if(reverse_s[i] == s[j]){
+                        if(j == 0) cur_len[j] = 1;
+                        else    cur_len[j] = pre_len[j-1] + 1;
+                    }
+                    else
+                        cur_len[j] = 0;
+                    if(cur_len[j] > max_len){
+                        max_len = cur_len[j];
+                        max_pos = j;
+                    }
+                }
+
+                pre_len = cur_len;
+            }
+            //cout << max_pos << "\t" << max_len << endl;
+            //cout << s[max_pos] << endl;
+            string result = s.substr(max_pos-max_len+1, max_len);
+            if(result == "")    result += s[0];
+            return result;
         }
-        return reverse_s;
-    }
 };
 
 #endif
